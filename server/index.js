700eeaf9fbe8486da82c908080e30c97f1f415c1
Ria-11 <ribbo456@gmail.com>
2026-04-14 00:34:53 +0530

    Added user model

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import User from "./models/User.js";
 
 dotenv.config();
 connectDB();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API running 🚀");
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
